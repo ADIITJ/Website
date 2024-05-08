04e174531213c3ec4f70ff1c1734bf4be400be72
ADIITJ <b22ai045@iitj.ac.in>
2024-05-08 14:25:02 +0530

    website completed

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
 const toggleButton = document.getElementById('toggle-button');
-const body = document.body;
+const linkedinLogo = document.getElementById('linkedin-logo');
+const githubLogo = document.getElementById('github-logo');
 
-toggleButton.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
+let currentMode = 'light'; // Default mode
+
+toggleButton.addEventListener('click', function() {
+    currentMode = currentMode === 'light'? 'dark' : 'light';
+    document.body.classList.toggle('dark-mode'); // Assuming you have a.dark-mode class for styling
+    updateSocialLogosAndButtonText(currentMode);
 });
+
+function updateSocialLogosAndButtonText(mode) {
+    if (mode === 'dark') {
+        linkedinLogo.src = 'linkedin_dark.png';
+        githubLogo.src = 'github_dark.png';
+        toggleButton.textContent = 'Light'; // Change button text to Light Mode
+    } else {
+        linkedinLogo.src = 'linkedin_light.png';
+        githubLogo.src = 'github_light.png';
+        toggleButton.textContent = 'Dark'; // Change button text to Dark Mode
+    }
+}
